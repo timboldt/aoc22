f434d5ed259090e584fff09fdc2ef08b630b5930
Tim Boldt <tim.boldt@gmail.com>
2022-11-19 19:48:23 -0400

    Basic framework implemented

---
+++ src/day00.rs
@@ -0,0 +1,19 @@
+fn parse(input: String) -> i32 {
+    42
+}
+
+fn part1(i: i32) -> i32 {
+ 99
+}
+
+fn part2(i: i32) -> i32 {
+    44
+   }
+   
+pub fn run(input: String) {
+    let parsed = parse(input);
+    let p1 = part1(parsed);
+    println!("Part 1: {}", p1);
+    let p2 = part2(parsed);
+    println!("Part 2: {}", p2);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fmt::Display;
+use std::{env, fs};
+
+mod day00;
+
+#[derive(Debug)]
+enum AocError {
+    MissingArg,
+    InvalidDay,
+}
+
+impl Display for AocError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for AocError {}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = env::args();
+    if args.len() != 2 {
+        println!("Usage: aoc22 <day>");
+        return Err(Box::new(AocError::MissingArg));
+    }
+    let raw_day: String = args.nth(1).ok_or(Box::new(AocError::InvalidDay))?;
+    let day: i32 = raw_day.parse()?;
+    let input = fs::read_to_string(format!("input/{:02}.txt", day))?;
+
+    println!("== Day {} ==", day);
+    match day {
+        0 => day00::run(input),
+        _ => println!("Invalid day: {}", raw_day),
+    };
+
+    Ok(())
 }
