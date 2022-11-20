e608224ec4cdd4c16e746bc8b50cad105f3df5f3
Tim Boldt <tim.boldt@gmail.com>
2022-11-20 13:02:23 -0800

    Basic template complete

--- src/day00.rs
+++ src/day00.rs
@@ -1,19 +1,61 @@
-fn parse(input: String) -> i32 {
-    42
+//  Copyright 2022 Google LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#![warn(clippy::all)]
+
+use std::error::Error;
+use std::num::ParseIntError;
+
+fn parse(input: &str) -> Result<Vec<i32>, ParseIntError> {
+    input.lines().map(|s| s.parse()).collect()
 }
 
-fn part1(i: i32) -> i32 {
- 99
+fn part1(vals: &[i32]) -> i32 {
+    vals.iter().sum()
 }
 
-fn part2(i: i32) -> i32 {
-    44
-   }
-   
-pub fn run(input: String) {
-    let parsed = parse(input);
-    let p1 = part1(parsed);
-    println!("Part 1: {}", p1);
-    let p2 = part2(parsed);
-    println!("Part 2: {}", p2);
+fn part2(vals: &[i32]) -> i32 {
+    let subtot: i32 = vals.iter().sum();
+    subtot * 2
+}
+
+pub fn run(input: &str) -> Result<(i32, i32), Box<dyn Error>> {
+    let parsed = parse(input)?;
+    let p1 = part1(&parsed);
+    let p2 = part2(&parsed);
+    Ok((p1, p2))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn parse_works() {
+        assert_eq!(vec![-1, 0, 42], super::parse("-1\n0\n42").unwrap());
+    }
+
+    #[test]
+    fn parse_works_with_extra_lf() {
+        assert_eq!(vec![42], super::parse("42\n").unwrap());
+    }
+
+    #[test]
+    fn part1_works() {
+        assert_eq!(1 + 2 + 3, super::part1(&vec![1, 2, 3]));
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(2 + 4 + 6, super::part2(&vec![1, 2, 3]));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+//  Copyright 2022 Google LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#![warn(clippy::all)]
+
 use std::error::Error;
 use std::fmt::Display;
 use std::{env, fs};
@@ -7,7 +23,7 @@ mod day00;
 #[derive(Debug)]
 enum AocError {
     MissingArg,
-    InvalidDay,
+    InvalidDay(String),
 }
 
 impl Display for AocError {
@@ -24,15 +40,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Usage: aoc22 <day>");
         return Err(Box::new(AocError::MissingArg));
     }
-    let raw_day: String = args.nth(1).ok_or(Box::new(AocError::InvalidDay))?;
+    let raw_day: String = args.nth(1).ok_or_else(|| Box::new(AocError::MissingArg))?;
     let day: i32 = raw_day.parse()?;
     let input = fs::read_to_string(format!("input/{:02}.txt", day))?;
 
+    let (p1, p2) = match day {
+        0 => day00::run(&input),
+        // TODO: Figure out what the magic syntax is here so that we don't have to return inside a match statement.
+        _ => return Err(Box::new(AocError::InvalidDay(raw_day))),
+    }?;
+
     println!("== Day {} ==", day);
-    match day {
-        0 => day00::run(input),
-        _ => println!("Invalid day: {}", raw_day),
-    };
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
 
     Ok(())
 }
