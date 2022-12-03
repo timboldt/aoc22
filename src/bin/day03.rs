111c618ac3dc3bac55bd872e5a2175f9cf63d712
Tim Boldt <tim.boldt@gmail.com>
2022-12-03 10:09:07 -0800

    Day 3 Part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -15,6 +15,7 @@
 #![warn(clippy::all)]
 
 use anyhow::Result;
+use itertools::Itertools;
 use std::collections::HashSet;
 use std::fs;
 
@@ -32,6 +33,16 @@ fn common_item(s1: &str, s2: &str) -> char {
     *(h1.intersection(&h2).into_iter().next().unwrap())
 }
 
+fn common_item_among_three(s1: &str, s2: &str, s3: &str) -> char {
+    let h1: HashSet<char> = s1.chars().collect();
+    let h2: HashSet<char> = s2.chars().collect();
+    // TODO: Horrible hack. There must be a way to collect directly into a HashSet.
+    let s1_2: String = h1.intersection(&h2).collect();
+    let h1_2: HashSet<char> = s1_2.chars().collect();
+    let h3: HashSet<char> = s3.chars().collect();
+    *(h1_2.intersection(&h3).into_iter().next().unwrap())
+}
+
 fn item_priority(ch: char) -> i32 {
     let v = match ch {
         'a'..='z' => u32::from(ch) - u32::from('a') + 1,
@@ -41,7 +52,7 @@ fn item_priority(ch: char) -> i32 {
     v.try_into().unwrap()
 }
 
-fn parse(input: &str) -> Vec<(String, String)> {
+fn parse1(input: &str) -> Vec<(String, String)> {
     input.lines().map(split_rucksack).collect()
 }
 
@@ -51,14 +62,26 @@ fn part1(vals: &[(String, String)]) -> i32 {
         .sum()
 }
 
-fn part2(_vals: &[(String, String)]) -> i32 {
-    42
+fn parse2(input: &str) -> Vec<(String, String, String)> {
+    let mut result: Vec<(String, String, String)> = vec![];
+    for chunk in &input.lines().chunks(3) {
+        let (x, y, z) = chunk.map(|s| s.trim()).collect_tuple().unwrap();
+        result.push((x.to_owned(), y.to_owned(), z.to_owned()));
+    }
+    result
+}
+
+fn part2(vals: &[(String, String, String)]) -> i32 {
+    vals.iter()
+        .map(|(s1, s2, s3)| item_priority(common_item_among_three(s1, s2, s3)))
+        .sum()
 }
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("input/03.txt")?;
-    let parsed = parse(&input);
+    let parsed = parse1(&input);
     let p1 = part1(&parsed);
+    let parsed = parse2(&input);
     let p2 = part2(&parsed);
 
     println!("Part 1: {}", p1);
@@ -78,13 +101,13 @@ mod tests {
 
     #[test]
     fn part1_works() {
-        let input = super::parse(SAMPLE);
+        let input = super::parse1(SAMPLE);
         assert_eq!(157, super::part1(&input));
     }
 
     #[test]
     fn part2_works() {
-        let input = super::parse(SAMPLE);
-        assert_eq!(45000, super::part2(&input));
+        let input = super::parse2(SAMPLE);
+        assert_eq!(70, super::part2(&input));
     }
 }
